4cd189f987efaffe2d8c32d864fdae89184e01a4
hellena254 <hellenah243@gmail.com>
2024-01-22 10:07:40 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using
+ * bubble_sort - Sorts an array of integers using
  *               the Bubble sort algorithm.
  *
  * @array: The array to be sorted
@@ -9,32 +9,32 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int temp;
+	int swap;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap the elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+	for (i = 0; i < size - 1; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap the elements */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
+				swap = 1;
+				print_array(array, size);
+			}
+		}
 
-        /* If no two elements were swapped by inner loop, the array is sorted */
-        if (swapped == 0)
-            break;
-    }
+		/* If no two elements were swapped by inner loop, the array is sorted */
+		if (swap == 0)
+			break;
+	}
 }
--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <sdtlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
